4e0611b221ce1a0fc403fc0e3e5d31e012936628
karl <enriquezkharl14@gmail.com>
2021-03-30 10:07:01 +0800

    Added some semi Login functionality

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,15 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAysnc");
 
 const User = require("../models/user");
+const passport = require("passport");
 
 router.get("/register", (req, res) => {
    res.render("users/register");
 });
 
-router.post("/register",wrapAsync(async (req, res) => {
+router.post(
+   "/register",
+   wrapAsync(async (req, res) => {
       try {
          const { email, username, password } = req.body;
          const user = new User({ email, username });
@@ -20,7 +23,16 @@ router.post("/register",wrapAsync(async (req, res) => {
          req.flash("error", err.message);
          res.redirect("/register");
       }
-    })
+   })
 );
+
+router.get("/login", (req, res) => {
+   res.render("users/login");
+});
+
+router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
+   req.flash("success", "Welcome Back!");
+   res.redirect("/campgrounds");
+});
 
 module.exports = router;
